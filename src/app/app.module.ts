4b3def041ed65a88c3db0f2b98cf75a85f134a0d
Diego Valenzuela <valenv.dg@gmail.com>
2019-07-25 10:42:56 -0600

    Project initialized. Added scripts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatCheckboxModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatStepperModule, MatTabsModule, MatButtonModule, MatIconModule, MatCardModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
